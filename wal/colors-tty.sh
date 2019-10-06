98e42b576747f073dc6a55786ea0208f3d692cfa
SiliconRaven <keystroke33@gamil.com>
2019-10-06 18:46:56 +0300

    ssd preperation

---
+++ wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0120819
+                 \e]P134358C
+                 \e]P24F368F
+                 \e]P3334E9D
+                 \e]P45E55A3
+                 \e]P59766AA
+                 \e]P6C165A6
+                 \e]P7afc2d5
+                 \e]P87a8795
+                 \e]P934358C
+                 \e]PA4F368F
+                 \e]PB334E9D
+                 \e]PC5E55A3
+                 \e]PD9766AA
+                 \e]PEC165A6
+                 \e]PFafc2d5
+                 \ec'
---
+++ wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#120819";
+static const char* selfgcolor   = "#afc2d5";
+static const char* normbgcolor  = "#4F368F";
+static const char* normfgcolor  = "#afc2d5";
+static const char* urgbgcolor   = "#34358C";
+static const char* urgfgcolor   = "#afc2d5";
