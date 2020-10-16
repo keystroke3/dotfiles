ecda7b849da95d9679e13017d17841fb6bb21683
keystroke3 <keystroke33@gmail.com>
2020-10-16 22:07:25 +0300

    regular backup

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-setxkbmap -option compose:ralt
-xinput --set-button-map 8 1 3 2 4 5 6 7
-xinput set-prop 11 331 0.7
-xinput set-prop 8 319 1
-xsetroot -cursor_name left_ptr
-dirmngr --daemon
+
+
+xlayoutdisplay -p eDP1 -o HDMI1 -d 100
 declare -a programs=(
 "ssh-agent" 
 # "blueman-tray"
 "sxhkd"
 "nm-applet"
-"xfce4-power-manager"
 "flameshot"
 "dunst"
 )
@@ -19,9 +15,10 @@ declare -a lazy_load=(
 "thunderbird"
 exec /usr/lib/kdeconnectd
 indicator-kdeconnect
+discord
+telegram-desktop
+code
 )
-
-$HOME/.config/polybar/launch.sh &
 sleep 2
 for program in "${programs[@]}"; do
    if pgrep $program; then
@@ -31,15 +28,18 @@ for program in "${programs[@]}"; do
    fi
 done 
 
-if pgrep -f "tauon";then 
-    :
-else 
-    python3 /opt/tauon-music-box/tauon.py %U &
-    bspc desktop -f 9
+$HOME/.config/polybar/launch.sh &
+# 
+mons_num=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -w)
+xrandr --output HDMI1 --left-of eDP1 --output eDP1 --mode 1920x1080 
+if [ $mons_num -eq 2 ]; then
+    bspc monitor eDP1 -d  6 7 8 9 0
+    bspc monitor HDMI1 -d 1 2 3 4 5
+else
+    bspc monitor eDP1 -d 1 2 3 4 5 6 7 8 9 0
 fi
-
-picom --config ~/.config/picom.conf &
-~/.redpaper/wallpaper.sh
+picom --daemon --config ~/dotfiles/bspwm/compton.conf
+~/.redpaper/wallpaper.sh $
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 sleep 20
 for program in "${lazy_load[@]}"; do
@@ -49,5 +49,17 @@ for program in "${lazy_load[@]}"; do
        $program &
    fi
 done 
-sleep 10
-py ~/dotfiles/bspwm/scripts/swallow.py &
+
+ if pgrep -f "tauon";then 
+     :
+ else 
+     python3 /opt/tauon-music-box/tauon.py %U &
+     bspc desktop -f 9
+ fi
+setxkbmap -option compose:ralt
+xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput set-button-map "Elan Touchpad" 1 3 2 4 5 6 7
+xmodmap -e 'keycode 135 = Super_R'
+xsetroot -cursor_name left_ptr
+dirmngr --daemon
+wmname LG3D
