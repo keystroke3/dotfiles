5aef57ce2d3bf3a6a94b2dfb6ce65bb66c3f0364
keystroke3 <keystroke33@gmail.com>
2020-12-13 13:04:42 +0300

    moved to a new picom fork and added configs to mpv

--- bspwm/scripts/compton-toggle.sh
+++ bspwm/scripts/compton-toggle.sh
@@ -3,5 +3,5 @@ if pgrep -x "picom" > /dev/null
 then
 	killall picom && notify-send "compton is off"
 else
-	picom -b --config ~/dotfiles/bspwm/compton.conf && notify-send "compton is on"
+	picom -b --config ~/.config/picom.conf && notify-send "compton is on"
 fi
