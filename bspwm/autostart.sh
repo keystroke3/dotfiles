5572cb125762c82db9f93271b64827aded1d55d6
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-09-25 17:33:41 +0300

    regular update

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -24,6 +24,8 @@ for program in "${programs[@]}"; do
 done 
 
 # 
+xsetroot -cursor_name left_ptr &
+dirmngr --daemon &
 mons_num=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -w)
 xrandr --output HDMI1 --left-of eDP1 --output eDP1 --mode 1920x1080 
 if [ $mons_num -eq 2 ]; then
@@ -34,7 +36,7 @@ else
 fi
 $HOME/.config/polybar/launch.sh &
 picom --daemon --config ~/.config/picom.conf
-~/.redpaper/wallpaper.sh &
+~/.cache/redpaper/wallpaper.sh &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 pkill -f minimizerd &
 minimizerd &
@@ -57,7 +59,5 @@ setxkbmap -option compose:ralt &
 xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1 &
 #xinput set-button-map "Elan Touchpad" 1 3 2 4 5 6 7 &
 #xmodmap -e 'keycode 135 = Super_R' &
-xsetroot -cursor_name left_ptr &
-dirmngr --daemon &
 #wmname LG3D &
 
