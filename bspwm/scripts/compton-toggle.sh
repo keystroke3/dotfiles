00c9b083ddcf3691042fc687e9cf4b57e9cbe674
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2022-02-19 00:35:58 +0300

    fixed a few things

--- bspwm/scripts/compton-toggle.sh
+++ bspwm/scripts/compton-toggle.sh
@@ -3,5 +3,5 @@ if pgrep -x "picom" > /dev/null
 then
 	killall picom && notify-send "compton is off"
 else
-	picom -b --config ~/.config/picom.conf && notify-send "compton is on"
+	picom --experimental-backends -b && notify-send "compton is on"
 fi
