4c52716be76a6b9c2776d5e2b697023fb5ccc303
Teddy Okello <37796862+keystroke3@users.noreply.github.com>
2022-06-21 12:58:11 +0300

    regular update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,58 +1,49 @@
 #!/bin/bash
-declare -a programs=(
-# "ssh-agent" 
-#"blueman-tray"
-"nm-applet"
-"flameshot"
-)
-dunst &
-declare -a lazy_load=(
-"thunderbird"
-copyq
-exec /usr/lib/kdeconnectd
-exec /usr/bin/kdeconnect-indicator
-LIBVA_DRIVER_NAME=i915 discord \
-     --ignore-gpu-blocklist \
-     --disable-features=UseOzonePlatform \
-     --enable-features=VaapiVideoDecoder \
-     --use-gl=desktop \
-     --enable-gpu-rasterization \ 
-     --enable-zero-copy
 
-#code
+# LIBVA_DRIVER_NAME=i914 discord \
+#       --ignore-gpu-blocklist \
+#       --disable-features=UseOzonePlatform \
+#       --enable-features=VaapiVideoDecoder \
+#       --use-gl=desktop \
+#       --enable-gpu-rasterization \ 
+#       --enable-zero-copy
+
+LOG_DIR=~/logs/autostart
+declare -a programs=(
+nm-applet
+flameshot
+dunst
 )
-sleep 2
+declare -a lazy_load=(
+thunderbird
+copyq
+kdeconnectd
+kdeconnect-indicator
+discord
+)
 for program in "${programs[@]}"; do
    if pgrep $program; then
        :
    else
-       $program &
+       $program &> $LOG_DIR/$program &
    fi
 done 
 
-# 
 xsetroot -cursor_name left_ptr &
-dirmngr --daemon &
-~/.config/bspwm/scripts/switch-displays &
-picom --experimental-backends --daemon 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+dirmngr --daemon &> $LOG_DIR/xsetroot
+~/.config/bspwm/scripts/switch-displays wm &> $LOG_DIR/switch
+picom --experimental-backends --daemon --log-level WARN --log-file $LOG_DIR/picom
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &> $LOG_DIR/polkit &
+
 sleep 10
 for program in "${lazy_load[@]}"; do
    if pgrep $program; then
-       return 0
+       :
    else
-       $program &
+       $program &> $LOG_DIR/$program &
    fi
 done 
 
-# if pgrep -f "tauon";then 
-#     :
-# else 
-#     python3 ~/Projects/TauonMusicBox/tauon.py %U &
-#     bspc desktop -f 9
-# fi
 setxkbmap -option compose:ralt &
-#xinput set-button-map "Elan Touchpad" 1 3 2 4 5 6 7 &
-#xmodmap -e 'keycode 135 = Super_R' &
-#wmname LG3D &
+wmname LG3D &
 
