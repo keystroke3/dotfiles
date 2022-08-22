c9cf6900d22c694c84607388dc50d9fc99e0eafc
Teddy Okello <37796862+keystroke3@users.noreply.github.com>
2022-08-22 04:16:20 +0300

    Backup before qtile

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,43 +1,34 @@
 #!/bin/bash
 
-# LIBVA_DRIVER_NAME=i914 discord \
-#       --ignore-gpu-blocklist \
-#       --disable-features=UseOzonePlatform \
-#       --enable-features=VaapiVideoDecoder \
-#       --use-gl=desktop \
-#       --enable-gpu-rasterization \ 
-#       --enable-zero-copy
-
 LOG_DIR=~/logs/autostart
 declare -a programs=(
 nm-applet
 flameshot
 dunst
+copyq
 )
 declare -a lazy_load=(
 thunderbird
-copyq
 kdeconnectd
 kdeconnect-indicator
-discord
 )
 for program in "${programs[@]}"; do
-   if pgrep $program; then
+   if pgrep -f $program; then
        :
    else
        $program &> $LOG_DIR/$program &
    fi
 done 
 
+gnome-keyring-daemon --start &
 xsetroot -cursor_name left_ptr &
 dirmngr --daemon &> $LOG_DIR/xsetroot
 ~/.config/bspwm/scripts/switch-displays wm &> $LOG_DIR/switch
 picom --experimental-backends --daemon --log-level WARN --log-file $LOG_DIR/picom
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &> $LOG_DIR/polkit &
 
 sleep 10
 for program in "${lazy_load[@]}"; do
-   if pgrep $program; then
+   if pgrep -f $program; then
        :
    else
        $program &> $LOG_DIR/$program &
@@ -46,4 +37,19 @@ done
 
 setxkbmap -option compose:ralt &
 wmname LG3D &
-
+if pgrep -f 'discord';then
+	:
+else
+LIBVA_DRIVER_NAME=i914 discord \
+      --ignore-gpu-blocklist \
+      --disable-features=UseOzonePlatform \
+      --enable-features=VaapiVideoDecoder \
+      --use-gl=desktop \
+      --enable-gpu-rasterization \ 
+      --enable-zero-copy &
+fi
+if pgrep -f 'qjackctl';then
+	:
+else
+	pw-jack qjackctl &
+fi
